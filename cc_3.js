35c0ea0e618bdc3c96505434eb8ac0e9520e9260
Alexander Curtis <ajcurtis@usf.edu>
2024-09-08 23:36:19 -0400

    Develop findTopandBottomPerformers Function

--- cc_3.js
+++ cc_3.js
@@ -16,3 +16,23 @@ function determinePerformanceRating(averageSales) {
         else if (averageSales >= 4000) {return "Satisfactory"; } 
         else {return "Needs Improvement"; }
 }
+
+//Task 3: Create a Function to Identify Top and Bottom Performers
+function findTopAndBottomPerformers(salespeople) {
+    let topPerformer = { name: '', totalSales: 0 };
+    let bottomPerformer = { name: '', totalSales: Infinity };
+    
+    for (let i = 0; i < salespeople.length; i++) {
+    let person = salespeople[i];
+    let totalSales = 0;
+    
+    for (let j = 0; j < person.sales.length; j++) {
+    totalSales += person.sales[j]; }
+
+    if (totalSales > topPerformer.totalSales) {topPerformer = { name: person.name, totalSales: totalSales };}
+    if (totalSales < bottomPerformer.totalSales) {bottomPerformer = { name: person.name, totalSales: totalSales };}
+    }
+}
+
+//Task 4: Combine Functions to Generate a Performance Report
+
