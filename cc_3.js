d84f70588d575ce1f0c7f6035442a8bc0929f0f8
Alexander Curtis <ajcurtis@usf.edu>
2024-09-08 23:13:51 -0400

    Create determine Performance Rating function

--- cc_3.js
+++ cc_3.js
@@ -9,4 +9,10 @@ let sales = [10000, 15000, 25000];
 let averageSales = calculateAverageSales(sales);
 console.log("Average sales: $" + averageSales);
 
-//Task 2: 
+//Task 2: Create a Function to Determine Performance Rating
+function determinePerformanceRating(averageSales) {
+    if (averageSales > 10000) {return "Excellent";} 
+        else if (averageSales >= 7000) {return "Good";} 
+        else if (averageSales >= 4000) {return "Satisfactory"; } 
+        else {return "Needs Improvement"; }
+}
